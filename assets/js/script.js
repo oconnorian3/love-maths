7d7e5a37ff8ed04ae1fb0880ad69018ab44d1d06
oconnorian3 <oconnorian3@gmail.com>
2022-11-04 21:48:08 +0000

    Added function to gemerate random numbers

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,8 +16,14 @@ document.addEventListener("DOMContentLoaded", function() {
    }
 
 })
-
+/** The main game "loop" called when the script is forst loaded
+ * and after the user's answer has been processsed
+ */
 function runGame () {
+
+    // Creates random numbers between 1 and 25
+    let num1 = Math.floor(Math.random() * 25) +1;
+    let num2 = Math.floor(Math.random() * 25) +1;
 
 }
 
